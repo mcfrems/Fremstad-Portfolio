5f9afb0f5cd87798e69c3e0bf4ee0b4eab81c98c
mcfrems <matthew.fremstad@gmail.com>
2023-02-20 21:37:54 -0600

    nav bar revision

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenuLeft = document.querySelector(".nav-menu-left");
+const navMenuRight = document.querySelector(".nav-menu-right");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    navMenuLeft.classList.toggle("active");
+    navMenuRight.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+    navMenuLeft.classList.remove("active");
+    navMenuRight.classList.remove("active");
 }))
